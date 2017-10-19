8961e428acad0749240cad8b3778f65357dc0272
Kst <kst404@gmail.com>
2017-10-19 17:35:58 +0300

    updated TaskViewController from master

--- dangerous-room/Tasks/TaskViewController.swift
+++ dangerous-room/Tasks/TaskViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 
 class TaskViewController: UIViewController {
     var started = false
@@ -18,34 +17,7 @@ class TaskViewController: UIViewController {
     @IBOutlet weak var taskDateLabel: UILabel!
     @IBOutlet weak var taskTimeLabel: UILabel!
     
-    var taskToEdit: Events? {
-        didSet {
-            updateViewsContent()
-        }
-    }
-    
-    func updateViewsContent() {
-        guard let event = taskToEdit else { return }
-        guard let _ = taskDateLabel else { return }
-
-        let dateFormatter:DateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM/dd/YY" // "HH:mm"
-        
-        let timeFormatter = DateFormatter()
-        timeFormatter.timeStyle = DateFormatter.Style.short
-        
-        let formattedDate = dateFormatter.string(from: event.date!)
-        taskDateLabel.text = "\(formattedDate)"
-        
-        let startDate = dateFormatter.string(from: event.date!)
-        let startTime = timeFormatter.string(from: event.date!)
-        let endTime = timeFormatter.string(from: event.date!.addingTimeInterval(TimeInterval(event.duration)))
-        
-        taskDateLabel.text = startDate
-        taskDescriptionLabel.text = event.event_description
-        taskTimeLabel.text = "\(startTime) - \(endTime)"
-        //            self.timer = DangerousTimer(duration: TimeInterval(task.duration), onTick: self.tick)
-    }
+    var taskToEdit: Events?
     
     var timer: DangerousTimer?
     
@@ -56,50 +28,15 @@ class TaskViewController: UIViewController {
         playButton.layer.masksToBounds = true
         playButton.layer.borderWidth = 1.0
         playButton.layer.borderColor = playButton.tintColor.cgColor
-<<<<<<< HEAD
-
-//        if let event = taskToEdit {
-//            let dateFormatter:DateFormatter = DateFormatter()
-//            dateFormatter.dateFormat = "MM/dd/YY" // "HH:mm"
-//
-//            let timeFormatter = DateFormatter()
-//            timeFormatter.timeStyle = DateFormatter.Style.short
-//
-//            let formattedDate = dateFormatter.string(from: event.date!)
-//            taskDateLabel.text = "\(formattedDate)"
-//
-//            let startDate = dateFormatter.string(from: event.date!)
-//            let startTime = timeFormatter.string(from: event.date!)
-//            let endTime = timeFormatter.string(from: event.date!.addingTimeInterval(TimeInterval(event.duration)))
-//
-//            taskDateLabel.text = startDate
-//            taskDescriptionLabel.text = event.event_description
-//            taskTimeLabel.text = "\(startTime) - \(endTime)"
-////            self.timer = DangerousTimer(duration: TimeInterval(task.duration), onTick: self.tick)
-//        }
-
-        updateViewsContent()
-        print("!!! !!! !!! viewDidLoad")
-=======
         
         self.updateUI()
     }
     
     func updateUI() {
->>>>>>> master
         if let event = taskToEdit {
-            print("!!! !!! !!! event = taskToEdit", event)
+            let dateFormatter:DateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/YY" // "HH:mm"
             
-<<<<<<< HEAD
-            NotificationCenter.default.addObserver(self, selector: #selector(respondToMOCChanges(_:)), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: nil)
-            ContextObserver.debugOutput = true
-            let observer = ContextObserver(context: event.managedObjectContext!)
-            NotificationCenter.default.addObserver(observer, selector: #selector(observer.managedObjectDidChange(_:)), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: nil)
-            observer.add().block { [weak self] object, type, keys in //.filter(taskToEdit)
-                print("!!! !!! !!! ContextObserver !!! !!! !!!", object)
-                self?.taskToEdit = object as? Events
-            }
-=======
             let timeFormatter = DateFormatter()
             timeFormatter.timeStyle = DateFormatter.Style.short
             
@@ -113,18 +50,9 @@ class TaskViewController: UIViewController {
             taskDateLabel.text = startDate
             taskDescriptionLabel.text = event.event_description
             taskTimeLabel.text = "\(startTime) - \(endTime)"
->>>>>>> master
         }
     }
     
-    @objc
-    func respondToMOCChanges(_ notification: Notification) {
-        guard let changedObjects = notification.userInfo?[NSUpdatedObjectsKey] as? Set<NSManagedObject> else { return }
-        
-        print("respondToMOCChanges", changedObjects.first?.managedObjectContext === MeteorCoreData.stack.managedObjectContext, changedObjects.first?.managedObjectContext === MeteorCoreData.stack.backgroundContext, changedObjects.first?.managedObjectContext === MeteorCoreData.stack.managedObjectContext)
-//        detailItem = changedObjects.first as? Event
-    }
-
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         self.timer?.stop()
@@ -147,23 +75,23 @@ class TaskViewController: UIViewController {
         guard let segueIdentifier = segue.identifier else { return }
         
         switch segueIdentifier {
-            case "editTask":
-                if let destinationController = segue.destination as? UINavigationController,
-                    let destinationEditController = destinationController.viewControllers.first as? TaskEditTableViewController {
-                    destinationEditController.title = "Edit Task"
-                    destinationEditController.taskToEdit = taskToEdit
-                }
-            case "countdown":
-                segue.destination.modalPresentationStyle = .custom
-                segue.destination.transitioningDelegate = self
-                
-                if let taskTimerController = segue.destination as? TaskTimerViewController {
-                    taskTimerController.task = taskToEdit
-                    taskTimerController.aliveTimeinterval = 10//2 * 60
-                    taskTimerController.betweenAliveTimeinterval = 10
-                }
-            default:
-                return
+        case "editTask":
+            if let destinationController = segue.destination as? UINavigationController,
+                let destinationEditController = destinationController.viewControllers.first as? TaskEditTableViewController {
+                destinationEditController.title = "Edit Task"
+                destinationEditController.taskToEdit = taskToEdit
+            }
+        case "countdown":
+            segue.destination.modalPresentationStyle = .custom
+            segue.destination.transitioningDelegate = self
+            
+            if let taskTimerController = segue.destination as? TaskTimerViewController {
+                taskTimerController.task = taskToEdit
+                taskTimerController.aliveTimeinterval = 10//2 * 60
+                taskTimerController.betweenAliveTimeinterval = 10
+            }
+        default:
+            return
         }
     }
     
@@ -171,10 +99,11 @@ class TaskViewController: UIViewController {
         print("Unwind View")
         self.updateUI()
     }
-
+    
     // MARK: - Unused
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
